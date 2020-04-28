185145fccee983bae8b6cd01028f6a8713a96c66
Yar177 <yarsher.dev@gmail.com>
2020-04-27 23:39:58 -0400

    parse json done

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -28,39 +28,24 @@ public class JsonUtils {
             JSONObject jsonObj = new JSONObject(json);
             JSONObject nameObj = jsonObj.getJSONObject("name");
             String name = nameObj.getString("mainName");
-
             sandwich.setMainName(nameObj.getString("mainName"));
             sandwich.setAlsoKnownAs(parseJsonData(nameObj,"alsoKnownAs"));
-
-            //List<String> listObjs = parseJsonData(nameObj,"alsoKnownAs");
             sandwich.setPlaceOfOrigin(jsonObj.getString("placeOfOrigin"));
             sandwich.setDescription(jsonObj.getString("description"));
             sandwich.setImage(jsonObj.getString("image"));
-
-            sandwich.setIngredients(parseJsonData(nameObj,"ingredients"));
-
-
-
-
+            sandwich.setIngredients(parseJsonData(jsonObj,"ingredients"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-
-
-
         return sandwich;
     }
 
 
     public static List<String> parseJsonData(JSONObject obj, String pattern)throws JSONException {
-
         List<String> listObjs = new ArrayList<>();
         JSONArray jsonArray = obj.getJSONArray (pattern);
         for (int i = 0; i < jsonArray.length(); ++i) {
-
             final String site = String.valueOf(jsonArray.getString(i));
-
             listObjs.add(site);
         }
         return listObjs;
