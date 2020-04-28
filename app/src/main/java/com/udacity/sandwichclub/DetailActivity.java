2a6c4a1a1a5ac07810f15184579e819133e8b130
Yar177 <yarsher.dev@gmail.com>
2020-04-28 00:04:40 -0400

    populateUI()

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    Sandwich sandwich = new Sandwich();
+
+
+//    private List<String> alsoKnownAs = null;
+//    private String placeOfOrigin;
+//    private String description;
+//    private String image;
+//    private List<String> ingredients = null;
+
+    TextView alsoKnownAs, placeOfOrigin, description, ingredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +70,17 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        alsoKnownAs = (TextView) findViewById(R.id.also_known_tv);
+        alsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
+
+        placeOfOrigin = (TextView) findViewById(R.id.origin_tv);
+        placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+
+        description = (TextView) findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
+
+        ingredients = (TextView) findViewById(R.id.ingredients_tv);
+        ingredients.setText(sandwich.getIngredients().toString());
 
     }
 }
